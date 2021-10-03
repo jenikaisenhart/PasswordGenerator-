b9a2dd941c3c1b3d3fd9259f498c3f2d0dffb056
Kim Possible <jenikacisenhart@gmail.com>
2021-10-02 23:27:00 -0700

    code for password prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = upperCase.toLowerCase();
+var number = "0123456789";
+var symbol = "!@#$%^&*(){}[]=<>/,."
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+ var password = generatePassword(); 
+ var passwordText = document.querySelector("#password"); 
+ passwordText.value = password;
 }
+
+function generatePassword(){
+
+  var passwordLength = parseInt(prompt("How many characters would you like for your password? 8-128"));
+
+  var userInput= null;
+  var randomPassword = "";
+
+  if (passwordLength >= 8 && passwordLength <= 128) 
+
+  var confirmUpperCase = confirm ("Select OK if you would like uppercase letters in your password.");
+  var confirmLowercase = confirm ("Select OK if you would like lowercase letters in your password.");
+  var confirmSymbol = confirm ("Select OK if you would like a symbol in your password.");
+  var confirmNumbers = confirm ("Select OK if you would like numbers in your password.");
+
+  if(confirmUpperCase) {
+    userInput += upperCase;
+  }
+  if(confirmLowercase) {
+   userInput += lowerCase;
+  }
+  if(confirmSymbol) {
+    userInput += symbol;
+  }
+  if(confirmNumbers) {
+    userInput = number;
+  }
+  if (userInput = ! null); {
+  
+   for (var i = 0; i < passwordLength; i++) {
+    randomPassword += userInput.charAT(Math.floor(Math.random() * userInput.length));
+    passwordText.value = randomPassword;
+    }
+  }
+}
+  
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
